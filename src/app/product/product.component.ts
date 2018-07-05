205e1ad6daab1a4fd19f77515e2b676e5fb8498b
YuYEZhou <1182756902@qq.com>
2018-07-05 17:18:21 +0800

    reactive pipe

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService, Product } from '../shared/product.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -10,6 +12,10 @@ export class ProductComponent implements OnInit {
 
   private products: Product[];
 
+  private keyword: string;
+
+  private titleFilter: FormControl = new FormControl()
+
   private imgUrl = "http://placehold.it/320x150"
 
   constructor(private ProductService: ProductService) { 
@@ -17,6 +23,11 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {
     this.products = this.ProductService.getProducts();
+    this.titleFilter.valueChanges
+        .pipe(debounceTime(500))
+        .subscribe(
+          value => this.keyword = value
+        )
   }
 
 }
